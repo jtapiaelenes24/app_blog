10d6d5990910446cddac8c694742019601334837
JT <jtapiaelenes24@gmail.com>
2025-05-21 17:16:01 +0200

    Creada copia de seguridad del archivo welcome.blade + Creado componente de prueba en ShowPost.php + Extendida la vista del componente show-post en la vista welcome.blade + Comentada la extensión de livewire dentro de la vista app.blade

--- app/Livewire/ShowPost.php
+++ app/Livewire/ShowPost.php
@@ -6,6 +6,9 @@ use Livewire\Component;
 
 class ShowPost extends Component
 {
+
+    public $prueba = 'Mi proyecto con livewire';
+
     public function render()
     {
         return view('livewire.show-post');
--- resources/views/livewire/show-post.blade.php
+++ resources/views/livewire/show-post.blade.php
@@ -1,3 +1,7 @@
 <div>
     {{-- Stop trying to control. --}}
+
+    <h1 class="text-lg text-red-600">Hola Mundo</h1>
+    {{ $prueba }}
+
 </div>
